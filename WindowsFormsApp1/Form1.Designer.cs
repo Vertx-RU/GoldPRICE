6d20e5dcf50c532e0e75472a42c8580ba8a5fe65
Vertx <1243775862@qq.com>
2019-06-20 11:08:33 +0800

    v1.0.0

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.GoldPrice = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.price = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.percent = new System.Windows.Forms.Label();
+            this.ppercent = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GoldPrice
+            // 
+            this.GoldPrice.ContextMenuStrip = this.contextMenuStrip1;
+            this.GoldPrice.Icon = ((System.Drawing.Icon)(resources.GetObject("GoldPrice.Icon")));
+            this.GoldPrice.Text = "GoldPrice";
+            this.GoldPrice.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(97, 26);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(3, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RealTime Gold Price:";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // price
+            // 
+            this.price.AutoSize = true;
+            this.price.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.price.ForeColor = System.Drawing.Color.Lime;
+            this.price.Location = new System.Drawing.Point(164, 4);
+            this.price.Name = "price";
+            this.price.Size = new System.Drawing.Size(87, 21);
+            this.price.TabIndex = 1;
+            this.price.Text = "XXX per/g";
+            this.price.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.price.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.price.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(105, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 27);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Initializing...";
+            this.label2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.label2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.label2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Black;
+            this.chart1.BorderlineColor = System.Drawing.Color.Black;
+            chartArea3.BackColor = System.Drawing.Color.Black;
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(-63, 17);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.chart1.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Lime};
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.LabelBackColor = System.Drawing.Color.Black;
+            series3.LabelBorderColor = System.Drawing.Color.Black;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(518, 62);
+            this.chart1.TabIndex = 9;
+            this.chart1.Text = "chart1";
+            this.chart1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.chart1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.chart1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.price);
+            this.panel1.Controls.Add(this.percent);
+            this.panel1.Controls.Add(this.ppercent);
+            this.panel1.Controls.Add(this.chart1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(334, 63);
+            this.panel1.TabIndex = 10;
+            this.panel1.Visible = false;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // percent
+            // 
+            this.percent.AutoSize = true;
+            this.percent.Font = new System.Drawing.Font("微软雅黑", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.percent.ForeColor = System.Drawing.Color.Lime;
+            this.percent.Location = new System.Drawing.Point(285, 0);
+            this.percent.Name = "percent";
+            this.percent.Size = new System.Drawing.Size(40, 14);
+            this.percent.TabIndex = 2;
+            this.percent.Text = "percent";
+            this.percent.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.percent.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.percent.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // ppercent
+            // 
+            this.ppercent.AutoSize = true;
+            this.ppercent.Font = new System.Drawing.Font("微软雅黑", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ppercent.ForeColor = System.Drawing.Color.Lime;
+            this.ppercent.Location = new System.Drawing.Point(285, 10);
+            this.ppercent.Name = "ppercent";
+            this.ppercent.Size = new System.Drawing.Size(46, 14);
+            this.ppercent.TabIndex = 3;
+            this.ppercent.Text = "ppercent";
+            this.ppercent.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.ppercent.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.ppercent.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 5000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.checkBox1.FlatAppearance.CheckedBackColor = System.Drawing.Color.Black;
+            this.checkBox1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.checkBox1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox1.ForeColor = System.Drawing.Color.Lime;
+            this.checkBox1.Location = new System.Drawing.Point(1, 48);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(45, 16);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Drag";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(334, 63);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.7D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon GoldPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label price;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label percent;
+        private System.Windows.Forms.Label ppercent;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using WebKit;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        WebKit.WebKitBrowser browser = new WebKitBrowser();
+        WebKit.DOM.Element element;
+        WebKit.DOM.Element element2;
+        WebKit.DOM.Element element3;
+        WebKit.DOM.Element element4;
+        WebKit.DOM.Element element5;
+        WebKit.DOM.Element element6;
+        WebKit.DOM.Element element7;
+        private static bool IsDrag = false;
+        private int enterX;
+        private int enterY;
+        bool first = true;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Location = new Point(0, 0);
+            browser.Location = new Point(0,0);
+            browser.Width = 0;
+            browser.Height = 0;
+            this.Controls.Add(browser);
+            browser.Navigate("http://fund.eastmoney.com/002611.html?spm=aladin");
+            browser.DocumentCompleted += Browser_DocumentCompleted;
+            this.TopMost = true;
+            double min = 0;
+            double max = 0;
+            if (File.Exists("PriceList.txt"))
+            {
+                string[] tmp = File.ReadAllText("PriceList.txt").Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                for (int i = 0; i < tmp.Length; i++)
+                {
+                    if (min == 0 && max == 0)
+                    {
+                        min = Convert.ToDouble(tmp[i]);
+                        max = Convert.ToDouble(tmp[i]);
+                    }
+                    if (tmp[i] != "")
+                    {
+                        if (min > Convert.ToDouble(tmp[i]))
+                        {
+                            min = Convert.ToDouble(tmp[i]);
+                        }
+                        if (max < Convert.ToDouble(tmp[i]))
+                        {
+                            max = Convert.ToDouble(tmp[i]);
+                        }
+                        chart1.Series[0].Points.Add(Convert.ToDouble(tmp[i]));
+                    }
+                }
+                if (min != 0 && max != 0)
+                {
+                    chart1.ChartAreas[0].AxisY.Minimum = min;
+                    chart1.ChartAreas[0].AxisY.Maximum = max;
+                }
+            }
+            else
+            {
+                File.Create("PriceList.txt").Close();
+            }
+        }
+
+        private void Browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            label2.Visible = false;
+            panel1.Visible = true;
+            Getdata();
+            timer1.Start();
+            timer2.Start();
+        }
+        double newmax = 0;
+        double neemin = 0;
+        double nowprice = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Getdata();
+            browser.Reload();
+        }
+        private void Getdata()
+        {
+            element = browser.Document.GetElementById("gz_gsz");
+            element2 = browser.Document.GetElementById("gz_gszze");
+            element3 = browser.Document.GetElementById("gz_gszzl");
+            price.Text = Convert.ToDouble(element.TextContent) * 285 + " per/g";
+            percent.Text = element2.TextContent;
+            ppercent.Text = element3.TextContent;
+            if (neemin != 0)
+            {
+                if (Convert.ToDouble(element.TextContent) * 285 < neemin)
+                {
+                    neemin = Convert.ToDouble(element.TextContent) * 285;
+                }
+                if (Convert.ToDouble(element.TextContent) * 285 > newmax && neemin != 0 && Convert.ToDouble(element.TextContent) * 285 > neemin)
+                {
+                    newmax = Convert.ToDouble(element.TextContent) * 285;
+                }
+            }
+            else
+            {
+                neemin = Convert.ToDouble(element.TextContent) * 285;
+                nowprice = neemin;
+            }
+            if (neemin != 0 && newmax != 0)
+            {
+                chart1.ChartAreas[0].AxisY.Minimum = neemin;
+                chart1.ChartAreas[0].AxisY.Maximum = newmax;
+            }
+            if (first)
+            {
+                first = false;
+                string tmp=  File.ReadAllText("PriceList.txt");
+                File.WriteAllText("PriceList.txt", tmp+(Convert.ToDouble(element.TextContent) * 285).ToString() + "\r\n");
+            }
+            if (nowprice != Convert.ToDouble(element.TextContent) * 285)
+            {
+                chart1.Series[0].Points.Add(Convert.ToDouble(Convert.ToDouble(element.TextContent) * 285));
+                string tmp = File.ReadAllText("PriceList.txt");
+                File.WriteAllText("PriceList.txt", tmp + (Convert.ToDouble(element.TextContent) * 285).ToString() + "\r\n");
+                nowprice = Convert.ToDouble(element.TextContent) * 285;
+            }
+           
+
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            this.Close();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Getdata();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            IsDrag = true;
+            enterX = e.Location.X;
+            enterY = e.Location.Y;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsDrag&&checkBox1.Checked)
+            {
+                Left += e.Location.X - enterX;
+                Top += e.Location.Y - enterY;
+                this.TopMost = true;
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            IsDrag = false;
+            enterX = 0;
+            enterY = 0;
+            this.BringToFront();
+            this.TopMost = true;
+        }
+    }
+}
